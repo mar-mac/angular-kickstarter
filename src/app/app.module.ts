68f1a04cb4593785fbfb5b0b5b6cd57ad838da8f
Marco Maccio <marco.maccio@marmac.name>
2017-10-16 14:47:10 +0200

    Added PersonalInfo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SignInComponent } from './components/public/sign-in/sign-in.component';
 import { SignUpComponent } from './components/public/sign-up/sign-up.component';
 import { SignOutComponent } from './components/public/sign-out/sign-out.component';
 import { DashboardComponent } from './components/auth/dashboard/dashboard.component';
+import { PersonalinfoComponent } from './components/auth/personalinfo/personalinfo.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { DashboardComponent } from './components/auth/dashboard/dashboard.compon
     SignUpComponent,
     SignOutComponent,
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    PersonalinfoComponent
   ],
   imports: [
     BrowserModule,
