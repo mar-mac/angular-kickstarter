c3814ce1b9ff59e5070e476096ed137ed9fb429a
Marco Maccio <marco.maccio@marmac.name>
2017-10-16 14:32:03 +0200

    Added Dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './components/public/sign-in/sign-in.component';
 import { SignUpComponent } from './components/public/sign-up/sign-up.component';
 import { SignOutComponent } from './components/public/sign-out/sign-out.component';
+import { DashboardComponent } from './components/auth/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
     SignUpComponent,
-    SignOutComponent
+    SignOutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
