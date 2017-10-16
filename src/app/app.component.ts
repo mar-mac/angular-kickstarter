d6a928dc69c0349c3390456f2b242eef6326a986
Marco Maccio <marco.maccio@marmac.name>
2017-10-16 15:01:52 +0200

    Added First Draft of UI with Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
 })
 export class AppComponent {
   title = 'app';
+
+  isAuthenticated = true;
+
+  constructor(public snackBar: MatSnackBar) {}
+
+  signUp() {
+
+  }
+
+  signIn() {
+    this.isAuthenticated = true;
+    this.openSnackBar('Welcome back, Enjoy the App', 'Close');
+  }
+
+  signOut() {
+    this.isAuthenticated = false;
+    this.openSnackBar('Bye Bye, See you soon', 'Close');
+  }
+
+  private openSnackBar(snackbarMessage: string, snackbarAction: string) {
+    this.snackBar.open(snackbarMessage, snackbarAction, {
+      duration: 2000,
+    });
+  }
 }
