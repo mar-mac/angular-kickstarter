460944ffe577ee35792b37537338c9322bd4fa18
Marco Maccio <marco.maccio@marmac.name>
2017-10-16 15:01:12 +0200

    Added Routing Module to manage navigation between pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent   } from './components/public/home/home.component';
+import { SignUpComponent } from './components/public/sign-up/sign-up.component';
+import { SignInComponent } from './components/public/sign-in/sign-in.component';
+import { DashboardComponent } from './components/auth/dashboard/dashboard.component';
+import {PersonalinfoComponent} from './components/auth/personalinfo/personalinfo.component';
+import {NotificationsComponent} from './components/auth/notifications/notifications.component';
+import {MessagesComponent} from './components/auth/messages/messages.component';
+
+const routes: Routes = [
+  { path: '',               redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',           component: HomeComponent            },
+  { path: 'signup',         component: SignUpComponent          },
+  { path: 'signin',         component: SignInComponent          },
+  { path: 'dashboard',      component: DashboardComponent       },
+  { path: 'personalinfo',   component: PersonalinfoComponent    },
+  { path: 'notifications',  component: NotificationsComponent   },
+  { path: 'messages',       component: MessagesComponent        }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
